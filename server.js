a9f94d5b957e11ae829287f67a390c7ebc4f63e2
Eitan Rosenberg <eitanrosenberg@gmail.com>
2017-10-31 19:23:49 -0400

    [Server] Update server to actually serve our application instead of the API server.

--- server.js
+++ server.js
@@ -1,3 +1,25 @@
-require('babel-register');
+const express = require('express');
+const paths = require('./config/paths');
 
-require('./api/index.js');
+const {
+  NODE_ENV,
+  SERVER_PORT,
+} = process.env;
+
+
+const app = express();
+
+app.use(express.static(paths.staticPath));
+app.set('view engine', 'ejs');
+
+app.get('/marco', (req, res) => {
+  res.send('polo');
+});
+
+app.get('*', (req, res) => {
+  res.render(paths.indexViewPath);
+});
+
+app.listen(SERVER_PORT, () => {
+  console.log(`Listening on port ${SERVER_PORT}!`);
+});
